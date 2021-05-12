d74aaee5ad3e1023667c4988ba4241585ca47814
Brian Holden <brian.holden@bloomingdales.com>
2021-05-12 15:59:47 -0400

    Added contentful

--- services/contentfulClient.js
+++ services/contentfulClient.js
@@ -5,9 +5,9 @@ if (process.env.NODE_ENV !== 'production') {
 var contentful = require('contentful');
 
 var client = contentful.createClient({
-  accessToken: process.env.CONTENTFUL_API_TOKEN,
-  space: process.env.CONTENTFUL_SPACE_ID,
-  host: process.env.CONTENTFUL_HOST
+  accessToken: 'ehySY3_A2pmEYaa_K9KyEd9giDfLLYP9a9vBNvD3JyQ',
+  space: 'm7vb8l3s35b6',
+  host: 'cdn.contentful.com'
 })
 
 exports.client = client;
